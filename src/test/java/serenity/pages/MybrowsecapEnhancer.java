3aaeb63d62d1f9cfe530eafdd5bc9835df225be0
Subash Krishnappa <subkrish2@publicisgroupe.net>
2019-05-30 20:33:29 +0530

    jenkins

--- src/test/java/serenity/pages/MybrowsecapEnhancer.java
+++ src/test/java/serenity/pages/MybrowsecapEnhancer.java
@@ -21,8 +21,9 @@ public class MybrowsecapEnhancer implements BeforeAWebdriverScenario {
             desiredCapabilities.setCapability(CapabilityType.SUPPORTS_JAVASCRIPT,true);
             desiredCapabilities.setCapability("name",testOutcome.getStoryTitle() + " - " + testOutcome.getTitle());
             ChromeOptions options = new ChromeOptions();
-            options.addArguments("--disable-notifications");
-            options.addArguments("--disable-popup-blocking");
+            options.addArguments("--headless");
+			options.addArguments("--disable-notifications");
+			options.addArguments("--disable-popup-blocking");
             options.addArguments("--no-sandbox");
             options.addArguments("--disable-dev-shm-usage");
 
